3da0a984742bf46390a1b554588ec40089e4713b
Maks_Ovsianiuk <maksovsanyuk@gmail.com>
2018-12-04 17:31:46 +0200

    Initial

---
+++ js/data.js
@@ -0,0 +1,14 @@
+
+
+let header = document.getElementById('header')
+header.addEventListener("mouseover",function(){ 
+    if( header.classList.contains("main") === false){
+        header.classList.add("main");
+    }
+});
+
+header.addEventListener("mouseout",function(){
+    if( header.classList.contains("main") === true){
+        header.classList.remove("main");
+    }
+});
